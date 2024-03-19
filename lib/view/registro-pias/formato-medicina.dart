16a0b66ecf0f3d443980bbe9d1626d35afb8bb6f
Julio Vega <armandojulio82@gmail.com>
2024-03-18 19:01:46 -0500

    iniicio proyecto

--- lib/view/registro-pias/formato-medicina.dart
+++ lib/view/registro-pias/formato-medicina.dart
@@ -1,50 +1,127 @@
 import 'package:flutter/material.dart';
 
 class FormatoMedicinaScreen extends StatelessWidget {
-  final columns = [
-    'PERSONA',
-    'LUGAR',
-    'DX 01',
-    'DX 02',
-    'DX 03',
-    'DX 04',
-    'Ecografía',
-    'Teleconsulta por médico',
-    '...'
-  ];
-
-  // Esta lista debe ser poblada con tus datos
-  final data = [
-    ['ANGEL QUISPE YANARICO', 'Intramuro', 'J42', '', '', '', '0', '0', '...'],
-    // Más datos...
-  ];
+  final List<Map<String, dynamic>> data = List.generate(
+    10,
+    (index) => {
+      'name': 'ANGEL QUISPE YANARICO',
+      'place': 'Intramuro',
+      'dx01': 'DX Data $index',
+      // Add more data fields as necessary
+    },
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MEDICINA'),
-      ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: DataTable(
-            columns: getColumns(columns),
-            rows: getRows(data),
+    return SingleChildScrollView(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Columna fija a la izquierda
+          IntrinsicWidth(
+            child: Column(
+              children: [
+                _buildFixedHeader(),
+                ..._buildFixedRows(),
+              ],
+            ),
           ),
-        ),
+          // Columnas desplazables a la derecha dentro de un área desplazable
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Column(
+                children: [
+                  _buildScrollingHeader(), // Encabezado de las columnas desplazables
+                  SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: DataTable(
+                      columns: _createScrollingColumns(),
+                      rows: _createScrollingRows(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  List<DataColumn> getColumns(List<String> columns) =>
-      columns.map((String column) => DataColumn(label: Text(column))).toList();
+  Widget _buildScrollingHeader() {
+    // Crear encabezados para las columnas desplazables
+    return DataTable(
+      columns: _createScrollingColumns(),
+      rows: const [],
+    );
+  }
 
-  List<DataRow> getRows(List<List<String>> data) =>
-      data.map((List<String> row) {
-        final cells = row.map((cell) => DataCell(Text(cell))).toList();
+  Widget _buildFixedHeader() {
+    // Crea el encabezado fijo
+    return DataTable(
+      headingRowHeight: 56,
+      columns: [
+        DataColumn(label: Text('TIPO DOCUMENTO')),
+        DataColumn(label: Text('PERSONA')),
+      ],
+      rows: [], // No rows, only header
+    );
+  }
 
-        return DataRow(cells: cells);
-      }).toList();
+  List<Widget> _buildFixedRows() {
+    // Crea las filas fijas
+    return List.generate(
+      data.length,
+      (index) => DataRow(
+        cells: [
+          DataCell(Text('DNI: ${data[index]['dni']}')),
+          DataCell(Text(data[index]['name'])),
+        ],
+      ).toWidget(),
+    );
+  }
+
+  DataRow _createScrollingRow(Map<String, dynamic> rowData) {
+    // Crear una fila desplazable basada en los datos de la fila proporcionados
+    return DataRow(
+      cells: <DataCell>[
+        DataCell(Text(rowData['dx01'])),
+        DataCell(Text(rowData['dx02'])),
+        // Continuar agregando DataCell basado en los datos de la fila
+      ],
+    );
+  }
+
+  List<DataRow> _createScrollingRows() {
+    // Crear las filas desplazables basadas en los datos
+    return data.map((rowData) => _createScrollingRow(rowData)).toList();
+  }
+
+  List<DataColumn> _createScrollingColumns() {
+    // Crear las columnas desplazables para el DataTable
+    return <DataColumn>[
+      DataColumn(label: Text('DX 01')),
+      DataColumn(label: Text('DX 02')),
+      DataColumn(label: Text('DX 03')),
+      DataColumn(label: Text('DX 04')),
+      // ... agrega tantas columnas como necesites ...
+    ];
+  }
+}
+
+extension DataRowExtension on DataRow {
+  Widget toWidget() {
+    // Deberías cambiar el Text widget para mostrar el contenido real del DataCell.
+    return Row(
+      children: cells.map((cell) {
+        // Asumimos que cada celda tiene un Text widget como hijo
+        final textWidget = cell.child as Text;
+        return Container(
+          padding: EdgeInsets.all(8.0), // Padding para el contenido de la celda
+          child: Text(textWidget.data ?? ''), // Muestra el texto de la celda
+        );
+      }).toList(),
+    );
+  }
 }
